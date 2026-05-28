a2a84631845def833bae0181cd12c9e58d2c6913
slemanz <william_sleman@hotmail.com>
2026-05-28 20:44:26 -0300

    general(feat): add simple blinky

--- general/app/Src/main.c
+++ general/app/Src/main.c
@@ -1,10 +1,16 @@
 #include <stdint.h>
+#include "stm32f411xe.h"
 
 int main(void)
 {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+
+    GPIOC->MODER &= ~(3U << (13*2));
+    GPIOC->MODER |=  (1U << (13*2));
 
     while(1)
     {
-        
+        GPIOC->ODR ^= (1U << 13);
+        for (volatile uint32_t i = 0; i < 1000000; i++);
     }
 }
