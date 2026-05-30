ed6d823488c13ed06927568fd98cc8c46227f0b8
slemanz <william_sleman@hotmail.com>
2026-05-30 10:39:43 -0300

    general(feat): retarget printf

--- general/app/Src/main.c
+++ general/app/Src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "stm32f411xe.h"
 #include "fpu.h"
 #include "uart.h"
@@ -7,11 +8,14 @@ int main(void)
 {
     fpu_enable();
     debug_uart_init();
-    
+
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
     GPIOC->MODER &= ~(3U << (13*2));
     GPIOC->MODER |=  (1U << (13*2));
+
+    printf("Hello world!\r\n");
+    printf("Debug in on!\r\n");
 
     while(1)
     {
--- general/drivers/Src/uart.c
+++ general/drivers/Src/uart.c
@@ -9,17 +9,21 @@
 
 #define CR1_TE                      (1U << 3)
 #define CR1_UE                      (1U << 13)
+#define SR_TXE                      (1U << 7)
 
 static uint16_t compute_uart_bd(uint32_t periph_clk, uint32_t baudrate);
+static void uart_write(int ch);
+
+int __io_putchar(int ch)
+{
+    uart_write(ch);
+    return ch;
+}
 
 void debug_uart_init(void)
 {
     /* Enable clock access to GPIOA */
     RCC->AHB1ENR |= GPIOAEN;
-
-    /* Set the mode of PA2 to alternate function mode */
-    GPIOA->MODER &= ~(1U << 4);
-    GPIOA->MODER |=  (1U << 5);
 
     /* Set alternate funtion type to AF7 (UART2_TX) */
     GPIOA->AFR[0] |= (1U << 8);
@@ -27,17 +31,30 @@ void debug_uart_init(void)
     GPIOA->AFR[0] |= (1U << 10);
     GPIOA->AFR[0] &= ~(1U << 11);
 
+    /* Set the mode of PA2 to alternate function mode */
+    GPIOA->MODER &= ~(1U << 4);
+    GPIOA->MODER |=  (1U << 5);
+
     /* Enable clock access to UART2 */
     RCC->APB1ENR |= UART2EN;
 
     /* Configure uart baudrate */
-    compute_uart_bd(APB1_FREQ, DBG_UART_BAUDRATE);
+    USART2->BRR = compute_uart_bd(APB1_FREQ, DBG_UART_BAUDRATE);
 
     /* Configure transfer direction */
     USART2->CR1 = CR1_TE;
 
     /* Enable UART Module */
     USART2->CR1 |= CR1_UE;
+}
+
+static void uart_write(int ch)
+{
+    /* Make sure transmit data register is empty */
+    while(!(USART2->SR & SR_TXE)){}
+
+    /* Write to transmit data register */
+    USART2->DR = (ch & 0xFF);
 }
 
 static uint16_t compute_uart_bd(uint32_t periph_clk, uint32_t baudrate)
