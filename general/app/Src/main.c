acbe19743b886952dbf4d67d9009a5901c23cb2b
slemanz <william_sleman@hotmail.com>
2026-05-30 17:32:21 -0300

    general(feat): implement led functions

--- general/app/Src/main.c
+++ general/app/Src/main.c
@@ -1,27 +1,24 @@
 #include <stdint.h>
 #include <stdio.h>
-#include "stm32f411xe.h"
 #include "fpu.h"
 #include "uart.h"
 #include "timebase.h"
+#include "bsp.h"
 
 int main(void)
 {
     fpu_enable();
     debug_uart_init();
     timebase_init();
-
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-
-    GPIOC->MODER &= ~(3U << (13*2));
-    GPIOC->MODER |=  (1U << (13*2));
+    led_init();
 
     printf("Hello world!\r\n");
     printf("Debug in on!\r\n");
 
+
     while(1)
     {
-        GPIOC->ODR ^= (1U << 13);
+        led_toggle();
         tick_delay(1);
     }
 }
---
+++ general/drivers/Src/bsp.c
@@ -0,0 +1,33 @@
+#include "bsp.h"
+#include "stm32f411xe.h"
+
+#define PIN13           (1U << 13)
+#define LED_PIN         PIN13
+#define PIN0            (1U << 0)
+#define BTN_PIN         PIN0
+
+void led_init(void)
+{
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+
+    GPIOC->MODER &= ~(3U << (13*2));
+    GPIOC->MODER |=  (1U << (13*2));
+}
+
+void led_on(void)
+{
+    GPIOC->ODR &= ~(LED_PIN);
+}
+
+void led_off(void)
+{
+    GPIOC->ODR |= (LED_PIN);
+}
+
+void led_toggle(void)
+{
+    GPIOC->ODR ^= (LED_PIN);
+}
+
+void button_init(void);
+bool button_get_state(void);
