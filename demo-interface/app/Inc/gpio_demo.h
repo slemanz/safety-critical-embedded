cf2e3c969fffb2080e40e5fbf199888050980db8
slemanz <william_sleman@hotmail.com>
2026-06-08 18:08:33 -0300

    demo-interface(feat): describing the interface

--- demo-interface/app/Inc/gpio_demo.h
+++ demo-interface/app/Inc/gpio_demo.h
@@ -1,6 +1,48 @@
+/** 
+ * @file gpio_demo.h
+ * @brief GPIO driver demonstration interface
+ * 
+ * This file provides a demonstration interface for the safe GPIO driver
+ * allowing interaction via UART commnads
+ * 
+ * @version 1.0
+ */
 #ifndef INC_GPIO_DEMO_H
 #define INC_GPIO_DEMO_H
 
+#include "safe_status.h"
 
+/**
+ * @brief Maximum line length for commands
+ */
+#define GPIO_DEMO_MAX_LINE      (64U)
+
+
+/**
+ * @brief Initialize the GPIO demonstration
+ * 
+ * @return Status_t Operation status
+ * @retval STATUS_OK Initialization successful
+ * @retval STATUS_ERROR_INIT Initialization failed
+ */
+
+/**
+ * @brief Run the GPIO demonstration
+ * 
+ * This function should be called periodically to process user commands.
+ * 
+ * @return Status_t Operation status
+ * @retval STATUS_OK Processing successful
+ * @retval STATUS_ERROR_INIT Demo not initialized
+ */
+
+/**
+ * @brief Display help for available commands
+ * 
+ * @return Status_t Operation status
+ * @retval STATUS_OK Processing successful
+ * @retval STATUS_ERROR_INIT Demo not initialized
+ * @retval STATUS_ERROR_PARAM Invalid command
+ */
 
 #endif /* INC_GPIO_DEMO_H */
