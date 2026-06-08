3a2dea4d5ccc606485b14c1a96cb80203f6abaf9
slemanz <william_sleman@hotmail.com>
2026-06-08 18:11:27 -0300

    demo-interface(feat): define functions prototype

--- demo-interface/app/Inc/gpio_demo.h
+++ demo-interface/app/Inc/gpio_demo.h
@@ -25,6 +25,7 @@
  * @retval STATUS_OK Initialization successful
  * @retval STATUS_ERROR_INIT Initialization failed
  */
+Status_t GPIO_Demo_Init(void);
 
 /**
  * @brief Run the GPIO demonstration
@@ -35,6 +36,7 @@
  * @retval STATUS_OK Processing successful
  * @retval STATUS_ERROR_INIT Demo not initialized
  */
+Status_t GPIO_Demo_Run(void);
 
 /**
  * @brief Display help for available commands
@@ -42,7 +44,17 @@
  * @return Status_t Operation status
  * @retval STATUS_OK Processing successful
  * @retval STATUS_ERROR_INIT Demo not initialized
+ */
+Status_t GPIO_Demo_DisplayHelp(void);
+
+/**
+ * @brief Proccess a command from the user
+ * 
+ * @return Status_t Operation status
+ * @retval STATUS_OK Processing successful
+ * @retval STATUS_ERROR_INIT Demo not initialized
  * @retval STATUS_ERROR_PARAM Invalid command
  */
+Status_t GPIO_Demo_ProccessCommand(const char* command);
 
 #endif /* INC_GPIO_DEMO_H */
